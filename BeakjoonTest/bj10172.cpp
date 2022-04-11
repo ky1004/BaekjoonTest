64e2a7afc40a611853e143cb077190bdddec65bf
ky1004 <pengukim@naver.com>
2022-04-11 23:59:23 +0900

    update file

---
+++ BeakjoonTest/bj10172.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\" ^ \"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+
+	return 0;
+}
+
+
+// 개를 출력하시오
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+	
+// 출력하고자 하는 특수기호 앞에 백슬래시(\)
